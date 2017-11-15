18d153b4a8304fdfeb8ec29a4773ee72fa7b1d24
David Ghent <dghent@pivotal.io>
2017-11-15 10:12:44 -0800

    Initial commit with grid

---
+++ src/App.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react';
+import './App.css';
+import './flex-grid-lite.css'
+import img from './img/img.jpg';
+
+class App extends Component {
+  render() {
+    return (
+
+    <div className="container">
+
+        <div className="header row">
+            <h2>ONTARIO SKATEPARKS</h2>
+            <p>
+            A free skatepark directory for Ontario, Canada.
+            </p>
+        </div>
+
+        <div className="grid">
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 1" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 2" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 3" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 4" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 1" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 2" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 3" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 4" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 1" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 2" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 3" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 4" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 1" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 2" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 3" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 4" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 1" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 2" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 3" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 4" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 1" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 2" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 3" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 4" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 1" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 2" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 3" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 4" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 1" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 2" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 3" />
+            <p>Title</p>
+          </div>
+          <div className="col col-fixed">
+            <img src={img} alt="skatepark 4" />
+            <p>Title</p>
+          </div>
+        </div>
+
+    </div>
+
+    );
+  }
+}
+
+export default App;
